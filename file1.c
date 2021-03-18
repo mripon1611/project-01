578891e3e6da4888c0c9919a775eca9b3f399037
Md. Alwoadud Ripon <ar.ripon1611@gmail.com>
2021-03-19 01:16:34 +0600

    create a new func div

--- file1.c
+++ file1.c
@@ -3,6 +3,10 @@ void mul(int a, int b)
 {
     printf("\nMul = %d",(a*b));
 }
+void div(int a, int b)
+{
+    printf("\nMul = %d",(a/b));
+}
 int sub(int a, int b)
 {
     return a-b;
@@ -18,5 +22,6 @@ int main()
     printf("\nSum = %d",sum(2,3));
     printf("\nSub = %d\n",sub(5,2));
     mul(4,2);
+    div(9,3);
     return 0;
 }
