3906cd3dd1fdd9fd48a4171fb543ed9f1a1d78b7
asmshad <ashad172167@bscse.uiu.ac.bd>
2021-03-19 01:06:33 +0600

    mul added by coWorker

--- file1.c
+++ file1.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+void mul(int a, int b)
+{
+    printf("\nMul = %d",(a*b));
+}
 int sub(int a, int b)
 {
     return a-b;
@@ -13,5 +17,6 @@ int main()
     printf("hello ripon");
     printf("\nSum = %d",sum(2,3));
     printf("\nSub = %d\n",sub(5,2));
+    mul(4,2);
     return 0;
 }
